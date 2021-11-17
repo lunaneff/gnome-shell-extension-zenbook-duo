929196523958041383ad6db81c0b03712b63c247
Laurin Neff <laurin@laurinneff.ch>
2021-11-17 20:13:21 +0100

    Add a script to bump the version

---
+++ scripts/bump_version.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+OLD_VERSION=$(awk '/VERSION = / {print $3}' ./Makefile)
+NEW_VERSION=$((1 + OLD_VERSION))
+
+echo "Bumping version from $OLD_VERSION to $NEW_VERSION"
+sed -i "s/VERSION = [0-9][0-9]*/VERSION = $NEW_VERSION/" Makefile
+sed -i "s/\"version\": \"[0-9][0-9]*\"/\"version\": \"$NEW_VERSION\"/" metadata.json
+sed -i "s/\/\/VERSION: [0-9][0-9]*/\/\/VERSION: $NEW_VERSION/" polkit/org.gnome.shell.extensions.zenbook-duo.rules
+sed -i "s/VERSION=[0-9][0-9]*/VERSION=$NEW_VERSION/" scripts/installer.sh
+sed -i "s/VERSION=[0-9][0-9]*/VERSION=$NEW_VERSION/" scripts/screenpad
